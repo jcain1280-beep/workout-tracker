2e902ddad78f7c68093b29858483802e5df4c5c5
Jon Cain <jcain1280@gmail.com>
2026-03-28 11:57:27 -0400

    Initial Commit

---
+++ Workout Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Workout Tracker
+//
+//  Created by Jonathan cain on 3/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Workout Tracker/Workout_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_TrackerApp.swift
+//  Workout Tracker
+//
+//  Created by Jonathan cain on 3/28/26.
+//
+
+import SwiftUI
+
+@main
+struct Workout_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
